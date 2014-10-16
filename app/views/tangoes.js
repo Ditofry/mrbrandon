d33f3eb8440d763ab8f9b1fd7931efe712350b6f
Ditofry <brandon@cirrously.com>
2014-10-15 21:33:52 -0600

    svg modifications

--- app/views/tangoes.js
+++ app/views/tangoes.js
@@ -8,11 +8,18 @@ export default Ember.View.extend({
     var s = Snap("#tangoes"),
         height = this.get('height'),
         convert = function(y){ return height - y };
-    var ruby = s.polygon(0,convert(300),120,convert(480),320,convert(0)).attr({ fill: "red", stroke: "blue", strokeWidth: 3, opacity: 0.5 });
-    var js = s.polygon(0,convert(300),120,convert(480),300,convert(360)).attr({ fill: "red", stroke: "green", strokeWidth: 3, opacity: 0.5 });
-    var css3 = s.polygon(0,convert(300),300,convert(360),320,convert(0)).attr({ fill: "blue", stroke: "gray", opacity: 0.5 });
-    var HTML5 = s.polygon(300,convert(360),480,convert(480),320,convert(0)).attr({ fill: "green", stroke: "purple", opacity: 0.5 });
-    var git = s.polygon(480,convert(480),600,convert(300),320,convert(0)).attr({ fill: "black", stroke: "white", opacity: 0.5 });
-    var PostGres = s.polygon(300,convert(360),600,convert(300),320,convert(0)).attr({ fill: "purple", stroke: "yellow", opacity: 0.5 });
+    // var ruby = s.polygon(0,convert(300),120,convert(480),320,convert(0)).attr({ fill: "red", stroke: "gray", strokeWidth: 3, opacity: 0.7 });
+    // var js = s.polygon(0,convert(300),120,convert(480),300,convert(360)).attr({ fill: "red", stroke: "gray", strokeWidth: 3, opacity: 0.7 });
+    // var css3 = s.polygon(0,convert(300),300,convert(360),320,convert(0)).attr({ fill: "red", stroke: "gray", opacity: 0.7 });
+    // var HTML5 = s.polygon(300,convert(360),480,convert(480),320,convert(0)).attr({ fill: "#61fdba", stroke: "gray", opacity: 0.7 });
+    // var git = s.polygon(480,convert(480),600,convert(300),320,convert(0)).attr({ fill: "#61fdba", stroke: "gray", opacity: 0.7 });
+    // var PostGres = s.polygon(300,convert(360),600,convert(300),320,convert(0)).attr({ fill: "#61fdba", stroke: "gray", opacity: 0.7 });
+    var ruby = s.polygon(0,convert(300),120,convert(480),320,convert(0)).attr({ stroke: 'white'});
+    var js = s.polygon(0,convert(300),120,convert(480),300,convert(360)).attr({ stroke: 'white'});
+    var css3 = s.polygon(0,convert(300),300,convert(360),320,convert(0)).attr({ stroke: 'white'});
+    var HTML5 = s.polygon(300,convert(360),480,convert(480),320,convert(0)).attr({ stroke: 'white'});
+    var git = s.polygon(480,convert(480),600,convert(300),320,convert(0)).attr({ stroke: 'white'});
+    var PostGres = s.polygon(300,convert(360),600,convert(300),320,convert(0)).attr({ stroke: 'white'});
+    // var g1 = s.gradient("l(0, 0, 1, 1)#000-#f00-#fff");
   }
 });
